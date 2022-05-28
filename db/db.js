2ecc64aca67ab0aa2e76faac10a4d6f04fb7b32d
Shivaji z <99667776+CodEsHiVaz@users.noreply.github.com>
2022-05-29 02:23:24 +0530

    Data added

---
+++ db/db.js
@@ -0,0 +1,179 @@
+export default {
+  bio: {
+    about: {
+      text: [
+        "Hi &#128075;",
+        "I'm Shivaji Zirpe. I’m Aspiring Full Stack Web Developer at Masai School specialized in MERN Stack Applications. I did my undergrad from TISS Mumbai.",
+        "I'm a developer, geek and curious human besides being an OpenSource enthusiast. I am intrested in delivering solutions for interesting problems from startup and industry space.",
+        "I love to read, listen to music, and travel.",
+      ],
+    },
+    contact: {
+      text: [
+        "If you would like to get in touch with me, be it for exploring a technology, a business, or to just say hi, feel free to send me an email. My email address is: shivaji432000@gmail.com",
+      ],
+    },
+  },
+  skills: [
+    {
+      title: "Languages",
+      skillName: "JavaScript",
+      color: "1",
+      percentage: "70",
+    },
+    {
+      title: "Frameworks/Libraries",
+      skillName: " Reactjs",
+      color: "6",
+      percentage: "50",
+    },
+    {
+      title: "Backend",
+      skillName: "Nodejs, MongoDB",
+      color: "2",
+      percentage: "40",
+    },
+    {
+      title: "Clouds",
+      skillName: " Netlify",
+      color: "3",
+      percentage: "30",
+    },
+    {
+      title: "Design",
+      skillName: "HTML, CSS,",
+      color: "4",
+      percentage: "70",
+    },
+    {
+      title: "Design",
+      skillName: "Bootstrap, Chakra ui",
+      color: "4",
+      percentage: "70",
+    },
+    {
+      title: "Version Control",
+      skillName: "GitHub",
+      color: "7",
+      percentage: "70",
+    },
+    {
+      title: "Tools",
+      skillName: "Postman, Chrome DevTools",
+      color: "3",
+      percentage: "80",
+    },
+
+    {
+      title: "Editor",
+      skillName: "VS Code",
+      color: "6",
+      percentage: "70",
+    },
+  ],
+  projects: {
+    web: [
+      {
+        projectName: " Clone of Freshly.con",
+        image: "images/Screenshot (138).png",
+        summary:
+          "for a group project we Developed fully functional a web application taht gives lots voritey of food dishes user can add it into cart and place order with good looking  user interface   ",
+        preview: "https://github.com/akhilgonde/FreshlyCloning",
+        techStack: ["HTML", "CSS", "JavaScript", "Netlify"],
+      },
+      {
+        projectName: "Clone of straberrynet",
+        image: "images/Screenshot (139).png",
+        summary:
+          "strawberrynet is about Beauty,Skincare,Makeup,Perfume brand products that lets their customers have a hassle-free and convenient online shopping experience.",
+        preview: "https://github.com/KalashThakur/straberrynet",
+        techStack: ["HTML", "CSS", "JavaScript"],
+      },
+    ],
+    software: [],
+    android: [],
+    freelance: [],
+  },
+  experience: [],
+  education: [
+    {
+      title: "Full Stack Web Development ",
+      duration: "Dec 2021 - Aug 2022",
+      subtitle: "Masai School, Bengaluru",
+      details: [
+        "Worked on collabrative projects.",
+        "Devoloping Web-Application with frontend and backend ",
+      ],
+      tags: [
+        "Data Structures & Algorithms",
+        "Front-End",
+        "Database Management System",
+        "Back-End",
+        "MERN",
+        "Full Stack",
+      ],
+      icon: "graduation-cap",
+    },
+    {
+      title: "Bachelors in Printing and Packaging Technology",
+      duration: "June 2017 - Jan 2021",
+      subtitle: "Tata Institute of Social Sciences, Mumbai",
+      details: [],
+      tags: [
+        "Flexible Packaging",
+        "Offset",
+        "Packging Devlopment",
+        "food Packaging",
+      ],
+      icon: "graduation-cap",
+    },
+    {
+      title: "Class 11-12th in Science and Mathematics",
+      duration: `June 2015 - Mar 2017`,
+      subtitle: "State Board of Secondary Education, Maharashtra",
+      details: [],
+      tags: ["Physics", "Chemistry", "Mathematics", "Biology"],
+      icon: "book",
+    },
+  ],
+  footer: [
+    {
+      label: "Dev Profiles",
+      data: [
+        {
+          text: "GitHub",
+          link: "https://github.com/CodEsHiVaz/",
+        },
+      ],
+    },
+    {
+      label: "Resources",
+      data: [
+        {
+          text: "Enable Dark/Light Mode",
+          func: "enableDarkMode()",
+        },
+        {
+          text: "Print this page",
+          func: "window.print()",
+        },
+      ],
+    },
+    {
+      label: "Social Profiles",
+      data: [
+        {
+          text: "Linkedin",
+          link: "https://www.linkedin.com/in/shivaji-zirpe-92650919b/",
+        },
+      ],
+    },
+    {
+      label: "copyright-text",
+      data: [
+        "Made with &hearts; by Shivaji Zirpe.",
+        "&copy; No Copyrights. Feel free to use this template.",
+      ],
+    },
+  ],
+};
